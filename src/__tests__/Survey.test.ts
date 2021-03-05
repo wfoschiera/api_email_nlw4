f954894e9d97a11f57d50e19adca34ff6ec9b4dd
wfoschiera <wfoschiera@gmail.com>
2021-03-05 16:15:50 -0400

    incluido afterAll para dropar as tabelas apos cada teste

--- src/__tests__/Survey.test.ts
+++ src/__tests__/Survey.test.ts
@@ -1,3 +1,4 @@
+import { Connection, getConnection } from 'typeorm';
 import request from 'supertest';
 import { app } from '../app';
 
@@ -8,6 +9,12 @@ describe("Surveys", () =>  {
     beforeAll(async () => {
         const connection = await createConnection();
         await connection.runMigrations();
+    });
+    
+    afterAll( async () => {
+        const connection = getConnection();
+        await connection.dropDatabase();
+        await connection.close();
     });
 
     it("Should be able to create a new survey", async () => {
