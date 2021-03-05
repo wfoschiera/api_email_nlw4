007ac7adf012c3b91504d2a35849250ddb36c48f
wfoschiera <wfoschiera@gmail.com>
2021-03-05 16:39:58 -0400

    incluida classe personalizada de erro na App para tratamento

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,9 @@
-import express from 'express';
 import 'reflect-metadata';
+import express from 'express';
+import "express-async-errors"
+import { AppError } from './errors/AppErrors';
+import { NextFunction, Response } from 'express';
+import { Request } from 'express';
 import createConnection from "./database";
 import { router } from './routes';
 
@@ -20,4 +24,17 @@ const app = express();
 app.use(express.json());
 app.use( router );
 
+app.use(
+    (err: Error, request: Request, response: Response, _next: NextFunction) => {
+        if (err instanceof AppError) {
+            return response.status(err.statusCode).json({
+                message: err.message
+        });
+        
+    }
+    return response.status(500).json({
+        status: "Error",
+        message: `Internal server error ${err.message}`,
+    })
+})
 export { app };
