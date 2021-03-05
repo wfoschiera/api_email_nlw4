aa45fc21a4235dcf57a0d9c0c2cf137afe04522a
wfoschiera <wfoschiera@gmail.com>
2021-03-05 16:10:26 -0400

    criada rota answers e incluido yup para validacao de entrada

---
+++ src/controllers/AnswerController.ts
@@ -0,0 +1,40 @@
+import { getCustomRepository, Not, IsNull } from 'typeorm';
+import { Request } from 'express';
+import { Response } from 'express';
+import { SurveysUsersRepository } from '../repositories/SurveysUsersRepository';
+
+
+
+class AnswerController {
+
+    // Route params => parametros que compoe a rota (p.e. o "answer" o "1")
+    // Para recuperar varios route params: routes.get("/answer/:value/:nota/:batata")
+    // sempre : seguido do nome que sera dado a variavel
+
+    // query params - Utilizados para busca/paginaçao. Parametros nao obrigatorios. Mesmo que nao informar, a rota continua funcionando.
+    // chave=valor
+    async execute(request: Request, response:Response){
+
+        const { value } = request.params;
+        const { u } = request.query;
+
+        const surveysUsersRepository = getCustomRepository(SurveysUsersRepository);
+
+        const surveyUser = await surveysUsersRepository.findOne({
+            id: String(u)
+        });
+        if (!surveyUser) { 
+            return response.status(400).json({
+                error: "Survey User does not exists!"
+            })
+
+        }
+        surveyUser.value = Number(value);
+
+        await surveysUsersRepository.save(surveyUser);
+        
+        return response.json(surveyUser);
+    }
+}
+
+export {AnswerController}
--- src/routes.ts
+++ src/routes.ts
@@ -2,12 +2,16 @@ import { Router } from "express";
 import { SurveysController } from "./controllers/SurveysController";
 import { UserController } from "./controllers/UserController";
 import { SendMailController } from "./controllers/SendMailController";
+import { AnswerController } from "./controllers/AnswerController";
+import { NpsController } from "./controllers/NpsController";
 
 const router = Router();
 
 const userController = new UserController();
 const surveyController = new SurveysController();
 const sendMailController = new SendMailController();
+const answerController = new AnswerController();
+const npsController = new NpsController();
 
 // aqui a magica acontece. Liguei o metodo POST do caminho 
 // localhost/users ao controller responsável por criar usuários
@@ -19,5 +23,9 @@ router.get("/surveys", surveyController.show);
 
 router.post("/sendMail", sendMailController.execute);
 router.get("/sendMail", sendMailController.show);
+
+router.get("/answers/:value", answerController.execute);
+
+router.get("/nps/:survey_id", npsController.execute);
 
 export { router };
